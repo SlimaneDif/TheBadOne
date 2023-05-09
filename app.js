08fb597a74cbe24bf8f4c2df0b07699a42ac4e75
SlimaneDif <133021018+SlimaneDif@users.noreply.github.com>
2023-05-09 15:42:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let userInput = document.getElementById('userAge');
+let password = "2005"
+let linkIfCorrect = "https://github.com";
+let linkIfWrong = "https://dark.netflix.io/en";
+let redirectionLink = document.getElementById('redirect-link');
+let mainSite = window.open('index.html');
+let confirmationBtn  = document.getElementById('confirmBtn')
+
+if(userInput.value === password){
+    redirectionLink.href = linkIfCorrect
+}else{
+    setTimeout(mainSite.open(), 100)
+}
