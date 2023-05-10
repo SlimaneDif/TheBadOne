800ace7f1d5ee20b84cf88d0303dbe8625199053
SlimaneDif <133021018+SlimaneDif@users.noreply.github.com>
2023-05-10 20:50:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-let userInput = document.getElementById('userAge');
-let password = "2005"
-let linkIfCorrect = "https://github.com";
-let linkIfWrong = "https://dark.netflix.io/en";
-let redirectionLink = document.getElementById('redirect-link');
-let mainSite = window.open('index.html');
-let confirmationBtn  = document.getElementById('confirmBtn')
-
-confirmationBtn.addEventListener("click", () => {
-    window.open() * 99999
-})
+let salam = 17;
+while(salam < 500){
+  console.log("Fuck")
+};
